d8402ee8dc6fc8f2dacf0ac2be7dcca6eb72f393
luckysahani <luckythegreat4@gmail.com>
2015-03-31 15:05:07 +0530

    working on code

--- Assignment 2/mysol.cpp
+++ Assignment 2/mysol.cpp
@@ -4,29 +4,91 @@ Implementing a B+ Tree in C++
 
  */
 
-#include <iostream>
+#include <fstream>
 // #include<conio>
 #include <iomanip>
 #include <stdio.h>
-#include <string.h>
 #include <time.h>
+#include <string>
+#include <iostream>
 
 using namespace std;
+int max_keys;
 
-void input_init()
+void set_maxkey_value(string filename)
 {
+	string line;
+    ifstream myfile (filename.c_str());
+    if (myfile.is_open())
+    {
+        while (myfile.good())
+        {
+        	myfile >> max_keys;
+        }
+        myfile.close();
+    }
+    else 
+    {  
+        cout << "Unable to open file "<<filename<<endl; 
+    }
+    cout<<"Setting the maxkeys to "<<max_keys<<endl;
+}	
 
+
+void queries_init(string filename)
+{
+	// cout<<"The filename is "<<filename<<endl;
+
+	cout<<"All the queries have been processed\n";
+}
+
+void input_init(string filename)
+{
+	// cout<<"The filename is "<<filename<<endl;
+
+
+	cout<<"Points from the file "<<filename<< " has been inserted \n";
 }
 
 int main()
 {
-	string filename="assgn2_bplus_data.txt";
-
+	string input="assgn2_bplus_data.txt",queries="querysample.txt";
+	string file_maxkeys="bplustree.config";
 	cout<<"Processing.........\n";
+	set_maxkey_value(file_maxkeys);
 	cout<<"Inserting the points from assgn2_bplus_data.txt\n";
-	input_init();
+	input_init(input);
+	cout<<"Processing the queries\n";
+	queries_init(queries);
 	
 
 
 	return 0;
 }
+
+
+
+
+// void file_reader()
+// { 
+//     string line;
+//     ifstream myfile ("example.txt");
+//     if (myfile.is_open())
+//     {
+//         while (myfile.good())
+//         {
+//           stream_reader(myfile);
+//         }
+//         myfile.close();
+//     }
+//     else 
+//     {  
+//         cout << "Unable to open file"; 
+//     }
+// }
+
+// void stream_reader(istream& stream)
+// {
+//     getline (stream,line);
+//     cout << line << endl;
+// }
