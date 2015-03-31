f87507cf83a47310297ebb15bfbe66418462954e
luckysahani <luckythegreat4@gmail.com>
2015-04-01 00:21:24 +0530

    working on assignment 2

--- Assignment 2/try_b.cpp
+++ Assignment 2/try_b.cpp
@@ -6,15 +6,12 @@
 #include <string.h>
 using namespace std;
 
-int file_cnt = 3, max_elem  = 2;
+int file_cnt = 1, max_elem  = 5;
 char root[20] = "node_0.txt", fileinit[20] = "node_", fileformat[20] = ".txt", tempfile1[40]= "", tempfile2[40] = "";
 
 float split(char filename[], float* keyarr, char valarr[][20], bool isleaf ){
 	char temp[10] = "", temp1[10] = "";
 	tempfile2[0]= tempfile1[0] = '\0';
-	// strcpy(temp,to_string(file_cnt).c_str());
-	// strcat(tempfile1, fileinit), strcat(tempfile1 , temp),	strcat(tempfile1,fileformat);
-	// file_cnt++;
 	strcpy(tempfile1, filename);
 	strcpy(temp1,to_string(file_cnt).c_str());
 	strcat(tempfile2, fileinit), strcat(tempfile2 , temp1),	strcat(tempfile2,fileformat);
@@ -61,8 +58,6 @@ float insert(float key, char val[], char filename[]){
 	fin >> num_elem >> isleaf;
 	float temp_key;
 	char file[40];
-	// cout << isleaf << endl;
-	// return -1;
 	if(isleaf){
 		cout << "I am in leaf insert "<<endl;
 		float keyarr[num_elem+1];
@@ -99,76 +94,81 @@ float insert(float key, char val[], char filename[]){
 			for(int i = 0; i < num_elem +1;i++){
 				f << keyarr[i] << " " << valarr[i] << endl;
 			}
+			return -1;
 		}
 	}
 	else {
 		float keyarr[num_elem+1];
 		char valarr[num_elem+4][20];
-		int j=0;
-		for(int i=0;i<num_elem;i++,j++){	
+		int i,j=0;
+		for(i=0;i<num_elem;i++,j++){	
 			fin >> valarr[j] >> keyarr[j];
 			if(key < keyarr[j]){
-				float ret = insert(key, val, valarr[j]);
-				// cout << "I am here "<< valarr[j] <<endl;
-
-				// cout << ret << endl;
-				// return -1;
-				if(ret != -1){
-					strcpy(valarr[j+1],tempfile2);
-					strcpy(valarr[j],tempfile1);
-					keyarr[j+1] = keyarr[j];
-					keyarr[j] = ret;
-					j++,j++,i++;
-					// cout << valarr[2] << " Hello"<< endl;
-					for(;i<num_elem;i++,j++){
-						fin >> valarr[j] >> keyarr[j];
-					}
-					fin >> valarr[j];
-					fin.close();
-					if(num_elem == max_elem){
-						return split(filename,keyarr,valarr,isleaf);
-					}
-					else{
-						ofstream f;
-						f.open(filename,ofstream::out);
-						f << num_elem+1 << " 0\n";
-						for(int i=0;i<=num_elem;i++){
-							f << valarr[i] << keyarr[i];
-						}
-						f<<valarr[i];
-					}
-				}
-				else{
-					return -1;
-				}
+				break;
 			}
+		}
+		float ret = insert(key, val, valarr[j]);
+		if(ret != -1){
+			strcpy(valarr[j+1],tempfile2);
+			strcpy(valarr[j],tempfile1);
+			keyarr[j+1] = keyarr[j];
+			keyarr[j] = ret;
+			j++,j++,i++;
+			// cout << valarr[2] << " Hello"<< endl;
+			for(;i<num_elem;i++,j++){
+				fin >> valarr[j] >> keyarr[j];
+			}
+			fin >> valarr[j];
+			fin.close();
+		}
+		else{
+			return -1;
+		}
+		if(num_elem == max_elem){
+			return split(filename,keyarr,valarr,isleaf);
+		}
+		else{
+			ofstream f;
+			f.open(filename,ofstream::out);
+			f << num_elem+1 << " 0\n";
+			for(int i=0;i<=num_elem;i++){
+				f << valarr[i] << keyarr[i];
+			}
+			f<<valarr[num_elem + 1];
 		}
 	}
 
 }
-
-
 bool input(char filename[]){
 	int type;
 	float key;
 	ifstream fin ;
 	fin.open(filename, ifstream::in);
-	string val;
-	fin >> type;
-	fin >> key;
-	fin >> val;
+	char val[40];
+	//fin >> type;
+	for(int i=0;i<6;i++){
+		fin >> key;
+		fin >> val;
+		cout << key << " " << val << endl;
+		float ret = insert(key, val, root);
+		if(ret != -1){
+			char tempfile3[40] = "",temp1[40] = "";
+			strcpy(temp1,to_string(file_cnt).c_str());
+			strcat(tempfile3, fileinit), strcat(tempfile3 , temp1),	strcat(tempfile3,fileformat);
+			file_cnt++;
+			ofstream f;
+			f.open(tempfile3, ofstream::out);
+			f << "1 0\n";
+			f << tempfile1 << " " << ret << " " << tempfile2;
+		}
+
+	}
 	// if(type == 0)
-	// 	insert(key, val, root);
 }
-
-
 int main(){
-	float a[10];
-	a[1] = 1.1, a[0] = 1.2;
-	char b[20][20];
-	strcpy(b[0],"hello");
-	strcpy(b[1],"hello1");
-	char ch[20] = "hardik1";
-	cout << insert(1.16, ch, root);
+	char s[20];
+	cin >> s;
+	// cout << s;
+	input(s);
 	return 0;
 }
