b035a3dc8d420672e493cf725ac13a889b701056
luckysahani <luckythegreat4@gmail.com>
2015-03-31 14:24:49 +0530

    working on code

---
+++ Assignment 2/mysol.cpp
@@ -0,0 +1,32 @@
+/*
+
+Implementing a B+ Tree in C++
+
+ */
+
+#include <iostream>
+// #include<conio>
+#include <iomanip>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+using namespace std;
+
+void input_init()
+{
+
+}
+
+int main()
+{
+	string filename="assgn2_bplus_data.txt";
+
+	cout<<"Processing.........\n";
+	cout<<"Inserting the points from assgn2_bplus_data.txt\n";
+	input_init();
+	
+
+
+	return 0;
+}
