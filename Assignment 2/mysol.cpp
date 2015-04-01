cf399ba2143310fcaba9d82544c4c872c22b39e8
luckysahani <luckythegreat4@gmail.com>
2015-04-01 19:49:15 +0530

    working on assignment 2

--- Assignment 2/mysol.cpp
+++ Assignment 2/mysol.cpp
@@ -239,6 +239,7 @@ float insert_key(float key, string key_value, string filename)
 					myfile>> file_array[j];
 					cout << "calling insert with key ,key_value and file_array in is leaf \t"<<key<<key_value<<file_array[j]<<endl;
 					float return_value = insert_key(key, key_value, file_array[j]);
+					cout<<"\nhi\n";
 					if(return_value == -1)
 					{
 						myfile.close();
@@ -350,7 +351,6 @@ void input_init(string filename)
 			// cout<<"\n\n\nkey and value is"<< key << key_value << endl;
 			float return_value=insert_key(key,key_value,root); 
 			// cout<<"\n\n\nkey and value is"<< key << key_value<<" ........returned with "<<return_value << endl;
-			cout<<"\nhi\n";
 			if(return_value != -1)
 			{
 				string temp_child_3;
