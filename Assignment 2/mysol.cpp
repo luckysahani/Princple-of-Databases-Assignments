af373c6f30b4557104f88d32272dc317b45ef58a
luckysahani <luckythegreat4@gmail.com>
2015-04-01 18:47:07 +0530

    working on assignment 2

--- Assignment 2/mysol.cpp
+++ Assignment 2/mysol.cpp
@@ -267,7 +267,7 @@ float insert_key(float key, string key_value, string filename)
 				    file.close();
 				    return -1;
 				}
-				cout<<"exited non-leaf function \n\n"
+				cout<<"exited non-leaf function \n\n";
 				// else
 				// {
 				// 	return -1;
